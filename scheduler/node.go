66bb3cbd754bdd12aab8db931acf25d997f6a0d2
Bruce Ediger <bediger8@gmail.com>
2020-08-29 07:33:01 -0600

    Compiler enforces interface fit

--- scheduler/node.go
+++ scheduler/node.go
@@ -2,18 +2,23 @@ package scheduler
 
 import (
 	"fmt"
+	"jobscheduler/heap"
 	"time"
 )
 
 // SchedNode holds a function that we want to run at some
 // later time. Fits interface heap.Node so that instances
 // that fit Scheduler interface can hold it.
+// SchedNode fits interface heap.Node
 type SchedNode struct {
 	interval    int       // milliseconds until this function runs
 	desiredTime time.Time // desired time for function run
 	desiredNS   int64
 	fn          func()
 }
+
+// Get compiler to enforce interface compliance
+var _ heap.Node = (*SchedNode)(nil)
 
 func (sn *SchedNode) Value() int64 {
 	return sn.desiredNS
